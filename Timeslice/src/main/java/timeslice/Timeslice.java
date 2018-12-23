924db4d3e5cfbd7ab4e7c1bb3bac4b43738f80e5
Adrian Ng <adrian.j.ng@gmail.com>
2018-12-23 19:24:48 +0000

    HADOOP

--- Timeslice/src/main/java/timeslice/Timeslice.java
+++ Timeslice/src/main/java/timeslice/Timeslice.java
@@ -1,4 +1,5 @@
 package timeslice;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -17,55 +18,55 @@ import java.text.SimpleDateFormat;
 
 public class Timeslice extends Configured implements Tool {
 
-	static final SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+    static final SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
 
 
-	public static void printUsage(Tool tool, String extraArgsUsage) {
-		System.err.printf("Usage: %s [genericOptions] %s\n\n",
-				tool.getClass().getSimpleName(), extraArgsUsage);
-		GenericOptionsParser.printGenericCommandUsage(System.err);
-	}
+    public static void printUsage(Tool tool, String extraArgsUsage) {
+        System.err.printf("Usage: %s [genericOptions] %s\n\n",
+                tool.getClass().getSimpleName(), extraArgsUsage);
+        GenericOptionsParser.printGenericCommandUsage(System.err);
+    }
 
-	@Override
-	public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException  {
-		if (args.length != 2) {
-			printUsage(this, "<input> <output>");
-			return 1;
-		}
+    @Override
+    public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        if (args.length != 2) {
+            printUsage(this, "<input> <output>");
+            return 1;
+        }
 
-		Configuration config = getConf();
+        Configuration config = getConf();
 
-		FileSystem fs = FileSystem.get(config);
+        FileSystem fs = FileSystem.get(config);
 
-		Job job = Job.getInstance(config);
-		job.setJarByClass(this.getClass());
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		
-		// Delete old output if necessary
-		Path outPath = new Path(args[1]);
-		if (fs.exists(outPath)) 
-			fs.delete(outPath, true);
-		
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        Job job = Job.getInstance(config);
+        job.setJarByClass(this.getClass());
+        FileInputFormat.addInputPath(job, new Path(args[0]));
 
-		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Text.class);
+        // Delete old output if necessary
+        Path outPath = new Path(args[1]);
+        if (fs.exists(outPath))
+            fs.delete(outPath, true);
 
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(EdgeWritable.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		job.setMapperClass(TimesliceMapper.class);
-		job.setReducerClass(TimesliceReducer.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
 
-		job.setNumReduceTasks(22);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(EdgeWritable.class);
 
-		boolean status = job.waitForCompletion(true);
-		return status ? 0 : 1;
-	}
+        job.setMapperClass(TimesliceMapper.class);
+        job.setReducerClass(TimesliceReducer.class);
 
-	public static void main(String[] args) throws Exception {
-		int exitCode = ToolRunner.run(new Timeslice(), args);
-		System.exit(exitCode);
-	}
+        job.setNumReduceTasks(22);
+
+        boolean status = job.waitForCompletion(true);
+        return status ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new Timeslice(), args);
+        System.exit(exitCode);
+    }
 
 }
